1c51f260d3abc6a935a8867d7d1dafac7a2cf78c
naqi72 <naqi8756@gmail.com>
2024-09-30 10:06:28 +0530

    Update ocr_implementation.py

--- ocr_implementation.py
+++ ocr_implementation.py
@@ -1,27 +1,11 @@
 import easyocr
-import pytesseract
 from PIL import Image
 
 # Initialize EasyOCR for Hindi and English
 reader = easyocr.Reader(['hi', 'en'], gpu=False)
 
-
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  # Windows
-
-# Function to perform OCR using Tesseract
-def ocr_tesseract(image):
-    return pytesseract.image_to_string(image)
-
 # Function to perform OCR using EasyOCR
-def ocr_easyocr(image):
-    result = reader.readtext(image, detail=0)  # Pass the image directly
-    return ' '.join(result)
-
-# Hybrid OCR Function based on user selection
-def perform_ocr(image_path, ocr_model='easyocr'):
+def perform_ocr(image_path):
     image = Image.open(image_path)
-    
-    if ocr_model == 'tesseract':
-        return ocr_tesseract(image)  # Use the image directly
-    else:
-        return ocr_easyocr(image)  # Use the image directly
+    result = reader.readtext(image, detail=0)  # detail=0 returns just the text
+    return ' '.join(result)  # Join all the text into a single string
