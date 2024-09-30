89b4dc8cf37c6926cb76460e194af37589869ad1
naqi72 <naqi8756@gmail.com>
2024-09-30 08:21:41 +0530

    Update ocr_implementation.py

--- ocr_implementation.py
+++ ocr_implementation.py
@@ -1,1 +1,12 @@
+import easyocr
+import streamlit as st
+import json
+
+reader = easyocr.Reader(['en', 'hi'])
+
+def ocr_image(image):
+    result = reader.readtext(image, detail=0)
+    return result
+def extract_text_as_json(extracted_text):
+    return json.dumps({'extracted_text': extracted_text}, ensure_ascii=False)
 
